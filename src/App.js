6318d9adcd9a1f0e19f8e1b28d9a831f961b5045
Aqib-Shoaib <118466147+Aqib-Shoaib@users.noreply.github.com>
2024-04-08 16:39:35 +0500

    updated app.js and added Main.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "Ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: "dataRecieved", payload: data.questions })
+      )
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
